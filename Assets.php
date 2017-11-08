b87a95d71b278f598c60902f00dc8ad0346804ec
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-11-08 16:50:44 +0900

    Update Assets.php

--- Assets.php
+++ Assets.php
@@ -13,11 +13,6 @@ class Assets extends AssetBundle
         'forceCopy' => true
     ];
 
-   /* public $js = [
-    *    'v1.js'
-    * ]; 
-    */
-
     public function init()
     {
         $this->sourcePath = dirname(__FILE__) . '/assets';
