2d7998ae148e2f42c9348fd0a31d68df84b77f1b
Çağatay <cagatayldzz@gmail.com>
2017-12-29 13:42:26 +0300

    Create base.php

---
+++ messages/tr/base.php
@@ -0,0 +1,9 @@
+<?php
+
+	return [
+		'<strong>Twitch</strong> Chat' => '<strong>Twitch</strong> Sohbet',
+		'Discord Settings' => 'Twitch Ayarlar',
+		'Twitch URL:' => 'Twitch URL:',
+		'<strong>Twitch</strong> module configuration' => '<strong>Twitch</strong> modül yapılandırması',
+		'Save' => 'Kaydet',
+	];
