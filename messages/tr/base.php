563899bc51cd594a392322a39f75eb261420c679
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-12-30 07:28:00 +0900

    Fix: Formatting

--- messages/tr/base.php
+++ messages/tr/base.php
@@ -1,9 +1,8 @@
 <?php
-
-	return [
-		'<strong>Twitch</strong> Chat' => '<strong>Twitch</strong> Sohbet',
-		'Discord Settings' => 'Twitch Ayarlar',
-		'Twitch URL:' => 'Twitch URL:',
-		'<strong>Twitch</strong> module configuration' => '<strong>Twitch</strong> modül yapılandırması',
-		'Save' => 'Kaydet',
-	];
+return [
+    '<strong>Twitch</strong> Chat' => '<strong>Twitch</strong> Sohbet',
+    'Discord Settings' => 'Twitch Ayarlar',
+    'Twitch URL:' => 'Twitch URL:',
+    '<strong>Twitch</strong> module configuration' => '<strong>Twitch</strong> modül yapılandırması',
+    'Save' => 'Kaydet',
+];
