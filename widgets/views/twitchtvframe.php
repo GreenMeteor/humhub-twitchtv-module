88d92033c7a59b8854ca121302bc82f3025caf7d
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-03-25 12:03:07 -0400

    Update twitchtvframe.php

--- widgets/views/twitchtvframe.php
+++ widgets/views/twitchtvframe.php
@@ -3,26 +3,27 @@
 use yii\helpers\Url;
 use humhub\libs\Html;
 use humhub\models\Setting;
+use humhub\widgets\PanelMenu;
+use humhub\modules\twitchtv\Assets;
 
-\humhub\modules\twitchtv\Assets::register($this);
+Assets::register($this);
 ?>
 
 <div class="panel panel-default panel-twitchtv" id="panel-twitchtv">
-    <?= \humhub\widgets\PanelMenu::widget(['id' => 'panel-twitchtv']); ?>
+    <?= PanelMenu::widget(['id' => 'panel-twitchtv']); ?>
   <div class="panel-heading">
-    <?=Yii::t('TwitchtvModule.base', '<strong>Twitch</strong> Chat'); ?>
+    <?= Yii::t('TwitchtvModule.base', '<strong>Twitch</strong> Chat'); ?>
   </div>
   <div class="panel-body">
 
 <?= Html::beginTag('div') ?>
-<iframe frameborder="0" 
-        scrolling="no" 
-        id="TwitchtvFrame" 
-        src="<?= $twitchtvUrl; ?>" 
-        height="500" 
-        width="100%">
+<iframe frameborder="0"
+        scrolling="no"
+        id="chat_embed"
+        src="<?= $twitchtvUrl; ?>"
+        height="500"
+        width=100%>
 </iframe>
-
 <?= Html::endTag('div'); ?>
 </div>
 </div>
